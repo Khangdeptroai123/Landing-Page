7ce3266c8a2d54ed044b6242718e2f6823732c5c
khang <khangbinh261@gmail.com>
2026-05-02 12:47:35 +0700

    update

---
+++ assets/js/site-config.js
@@ -0,0 +1,27 @@
+window.SITE_CONFIG = {
+  imageBaseUrl: "https://res.cloudinary.com/dzdcrka45/image/upload/f_auto,q_auto/",
+  localImageBaseUrl: "assets/img/",
+  useCdnImages: true
+};
+
+window.getImageUrl = function (path, options = "") {
+  const config = window.SITE_CONFIG;
+
+  if (!config.useCdnImages) {
+    return config.localImageBaseUrl + path;
+  }
+
+  if (options) {
+    return config.imageBaseUrl.replace("/upload/", `/upload/${options}/`) + path;
+  }
+
+  return config.imageBaseUrl + path;
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("[data-img]").forEach(function (img) {
+    const path = img.getAttribute("data-img");
+    const size = img.getAttribute("data-size") || "";
+    img.src = getImageUrl(path, size);
+  });
+});
